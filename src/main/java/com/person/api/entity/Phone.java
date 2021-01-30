6ab7fe727382813780ed095c5f68044d7fcd3049
DionatanLeao <dionatan.ti@outlook.com>
2021-01-30 12:29:56 -0300

    Implementing JPA in the entity package

--- src/main/java/com/person/api/entity/Phone.java
+++ src/main/java/com/person/api/entity/Phone.java
@@ -1,5 +1,13 @@
 package com.person.api.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.person.api.enums.PhoneType;
 
 import lombok.AllArgsConstructor;
@@ -13,16 +21,22 @@ import lombok.NoArgsConstructor;
  *
  */
 
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private PhoneType type;
 	
+	@Column(nullable = false)
 	private String number;
 	
 }
